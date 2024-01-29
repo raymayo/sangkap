0882f8d895134afa825cf0a8271e41893c130fa6
raymayo <ormillada.jr@gmail.com>
2024-01-29 17:03:33 +0800

    css

--- src/components/RecipeView.jsx
+++ src/components/RecipeView.jsx
@@ -35,11 +35,51 @@ const RecipeView = () => {
 	}
 
 	return (
-		<div>
-			<h2>{recipeData.title}</h2>
-			<p>{recipeData.summary}</p>
-			{/* Add more details as needed */}
-		</div>
+		// <div>
+		// 	<h2>{recipeData.title}</h2>
+		// 	<p>{recipeData.summary}</p>
+		// 	{/* Add more details as needed */}
+		// </div>
+		<>
+			<h1 className="pt-10 text-5xl sm:text-6xl md:text-7xl lg:text-7xl">
+				Plate<span className="font-bold text-yellow-500">Mate</span>
+			</h1>
+			<div className="recipeContainer flex flex-col items-center mt-8 gap-y-8 lg:mx-32 lg:my-8 lg:pt-8">
+				<div className="flex flex-col items-center gap-y-4">
+					<h1 className="text-4xl font-bold text-center">{recipeData.title}</h1>
+					<p className="text-center text-xl w-2/3">{recipeData.summary}</p>
+				</div>
+				<div className="grid gap-2 w-full bg-white rounded-md shadow-md sm:grid-cols-1 lg:grid-cols-3 p-4 lg:p-0">
+					<div>
+						<img
+							src={recipeData.image}
+							alt={recipeData.title}
+							className="rounded-l-md"
+						/>
+					</div>
+					<div>
+						<div className="p-4">
+							<h1 className="font-bold text-3xl">Ingredients</h1>
+							<ul className="text-lg py-4">
+								{recipeData.ingredientList.map((item, index) => (
+									<li key={index}>•{item}</li>
+								))}
+							</ul>
+						</div>
+					</div>
+					<div className="p-4">
+						<h1 className="font-bold text-3xl">Instruction</h1>
+						<ul className="text-lg py-4">
+							{recipeData.instruction.map((item, index) => (
+								<li key={index}>
+									{index}. {item}
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
