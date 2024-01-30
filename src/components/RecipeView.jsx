156216a0f51c9afcd683faf34f8ea23b9d689432
raymayo <ormillada.jr@gmail.com>
2024-01-31 02:50:03 +0800

    commit

--- src/components/RecipeView.jsx
+++ src/components/RecipeView.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { PiBowlFoodFill } from 'react-icons/pi';
 
 const RecipeView = () => {
 	const { recipeId } = useParams(); // Access the recipe ID parameter from the URL
 	const [recipeData, setRecipeData] = useState(null);
+	const [count, setCount] = useState(0);
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
@@ -34,36 +36,51 @@ const RecipeView = () => {
 		return <p>Loading...</p>;
 	}
 
+	const goNextInstruction = () => {
+		const lastIndex = recipeData.instruction.length - 1;
+		console.log(lastIndex);
+
+		if (count === lastIndex) {
+			return;
+		}
+		setCount(count + 1);
+	};
+
+	const goPrevInstruction = () => {
+		if (count === 0) {
+			return;
+		}
+		setCount(count - 1);
+	};
 	return (
-		// <div>
-		// 	<h2>{recipeData.title}</h2>
-		// 	<p>{recipeData.summary}</p>
-		// 	{/* Add more details as needed */}
-		// </div>
 		<>
-			<h1 className="pt-8 text-5xl sm:text-6xl md:text-7xl lg:text-6xl font-bold">
-				plate<span className="text-green-500 italic">mate</span>
-			</h1>
-			<div className="recipeContainer grid gap-4 mt-8 rounded-md grid-cols-1 w-3/6 lg:w-4/6 md:grid-cols-1 lg:grid-cols-2 bg-white shadow-md">
+			<nav className="w-full grid p-6 justify-center">
+				<h1 className="flex text-5xl sm:text-6xl md:text-7xl lg:text-6xl font-base text-green-600">
+					<PiBowlFoodFill />
+					plate<span className="">mate</span>
+				</h1>
+			</nav>
+
+			<div className="recipeContainer grid gap-0 mt-8 rounded-lg grid-cols-1 w-3/6 lg:w-4/6 xl:w-7/12 md:w-5/6 w-5/6 md:grid-cols-1 lg:grid-cols-2 bg-white shadow-md lg:gap-4 xl:gap-4">
 				{/* md:bg-red-500 xl:bg-green-500 lg:bg-blue-500 */}
-				<div className="bg-white shadow-md">
+				<div className="bg-white rounded-l-md">
 					<img
 						src={recipeData.image}
 						alt={recipeData.title}
-						className="object-cover w-full h-full"
+						className="object-cover w-full h-44 sm:h-52 md:h-64 lg:h-full xl:h-full rounded-l-md"
 					/>
 				</div>
-				<div className="space-y-2 p-4 flex flex-col">
-					<h1 className="text-6xl font-medium uppercase lg:text-5xl xl:text-7xl">
+				<div className="space-y-2 p-4 flex flex-col lg:space-y-4 xl:space-y-4">
+					<h1 className="text-gray-900 text-6xl font-semibold uppercase md:text-7xl lg:text-5xl xl:text-6xl sm:w-5/6 lg:w-5/6 xl:w-5/6">
 						{recipeData.title}
 					</h1>
-					<p className="text-base font-normal poppin lg:text-base xl:text-xl">
+					<p className="text-gray-900 text-base font-normal poppin lg:text-base xl:text-lg">
 						{recipeData.summary}
 					</p>
-					<h1 className="font-medium text-3xl uppercase lg:text-3xl xl:text-4xl">
+					<h1 className="text-gray-900 font-semibold text-4xl uppercase sm:text-4xl md:text-4xl lg:text-4xl xl:text-4xl">
 						Ingredients
 					</h1>
-					<ul className="text-base lg:text-base xl:text-xl">
+					<ul className="text-gray-900 text-base lg:text-base xl:text-lg">
 						{recipeData.ingredientList.map((item, index) => (
 							<li key={index} className="poppin">
 								{item}
@@ -72,15 +89,27 @@ const RecipeView = () => {
 					</ul>
 				</div>
 			</div>
-			<div className="my-8 bg-white shadow-md p-4 rounded-md w-4/6 space-y-2">
-				<h1 className="font-medium text-3xl uppercase">Instruction</h1>
-				<ul className="text-lg">
+			{/* <div className="my-8 bg-white shadow-md p-4 rounded-md space-y-2 w-3/6 lg:w-4/6 xl:w-7/12 md:w-5/6 w-5/6">
+				<h1 className="font-semibold text-4xl uppercase">Instruction</h1>
+				<ul className="text-lg space-y-2">
 					{recipeData.instruction.map((item, index) => (
-						<li key={index} className="poppin">
-							{index}. {item}
-						</li>
+						<div key={index}>
+							<span className="text-gray-900 font-semibold">
+								Step {index + 1}:
+							</span>
+							<li className="poppin">{item}</li>
+						</div>
 					))}
 				</ul>
+			</div> */}
+			<div className="bg-white shadow-md p-4 rounded-md space-y-2 w-3/6 lg:w-4/6 xl:w-7/12 md:w-5/6 w-5/6 my-8">
+				<h1 className="font-semibold text-4xl uppercase">Instruction</h1>
+				<span className="text-gray-900 font-semibold">Step {count + 1}:</span>
+				<h1>{recipeData.instruction[count]}</h1>
+				<div className="flex justify-end gap-2">
+					<button onClick={goPrevInstruction}>prev</button>
+					<button onClick={goNextInstruction}>next</button>
+				</div>
 			</div>
 		</>
 	);
--- src/components/SearchSuggestion.jsx
+++ src/components/SearchSuggestion.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
+import { PiBowlFoodFill } from 'react-icons/pi';
 import { useNavigate } from 'react-router-dom';
 import { useAppState } from './AppStateContext';
 
@@ -154,94 +155,102 @@ const SearchSuggestion = () => {
 	};
 	return (
 		<>
-			<h1 className="pt-10 text-5xl sm:text-6xl md:text-7xl lg:text-6xl font-bold italic">
-				plate<span className="text-green-500">mate</span>
-			</h1>
-			<div className="container mb-8 rounded flex flex-col items-center">
-				<div id="recipeSearchBox" className="rounded p-8">
-					<div className="searchInput rounded">
-						<input
-							id="recipeSearchInput"
-							className="p-2 shadow-sm rounded-l border-b poppin"
-							type="text"
-							value={userInput}
-							onChange={onIngrType}
-							placeholder="Add Ingredients"
-						/>
-						<button
-							onClick={searchRecipe}
-							className="bg-green-500 text-black border-l-0 p-2 rounded-r shadow font-medium poppin">
-							Search
-						</button>
-					</div>
-					<div className="selectedItems rounded shadow bg-white poppin">
-						{ingredientArray.map((item, index) => (
-							<span className="itemEntered" key={index}>
-								{item}
-								<button onClick={deleteItemOnArray}>x</button>
-							</span>
-						))}
-					</div>
-					{userInput && (
-						<div className="suggestionBox rounded shadow bg-white poppin">
-							{filteredIngr.map((suggestion, index) => (
-								<span
-									key={index}
-									onClick={getSelectedIngr}
-									className="suggestion">
-									{suggestion}
+			<div className="bg-gray-200">
+				<nav className="w-full grid p-6 justify-center">
+					<h1 className="flex text-5xl sm:text-6xl md:text-7xl lg:text-6xl font-base text-green-600">
+						<PiBowlFoodFill />
+						plate<span className="">mate</span>
+					</h1>
+				</nav>
+				<div className="container mb-8 rounded flex flex-col items-center bg-gray-200">
+					<div id="recipeSearchBox" className="rounded p-8">
+						<div className="searchInput rounded">
+							<input
+								id="recipeSearchInput"
+								className="p-2 shadow-sm rounded-l border-b poppin"
+								type="text"
+								value={userInput}
+								onChange={onIngrType}
+								placeholder="Add Ingredients"
+							/>
+							<button
+								onClick={searchRecipe}
+								className="bg-green-500 text-green-900 text-black border-l-0 p-2 rounded-r shadow font-medium poppin">
+								Search
+							</button>
+						</div>
+						<div className="selectedItems rounded shadow bg-white poppin">
+							{ingredientArray.map((item, index) => (
+								<span className="itemEntered" key={index}>
+									{item}
+									<button onClick={deleteItemOnArray}>x</button>
 								</span>
 							))}
 						</div>
+						{userInput && (
+							<div className="suggestionBox rounded shadow bg-white poppin">
+								{filteredIngr.map((suggestion, index) => (
+									<span
+										key={index}
+										onClick={getSelectedIngr}
+										className="suggestion">
+										{suggestion}
+									</span>
+								))}
+							</div>
+						)}
+					</div>
+					<div
+						id="recipeBox"
+						className="grid grid-cols-1 px-4 pt-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8 sm:gap-8 md:gap-8 lg:gap-6 xl:gap-6 items-start justify-center">
+						{displayProto}
+					</div>
+					{displayProto.length === 0 && (
+						<div
+							className="flex flex-col w-5/6 items-center gap-6 lg:w-4/6"
+							id="descBox">
+							<div className="desc p-4 rounded-md bg-white shadow">
+								<h1 className="text-2xl font-semibold text-green-500">
+									What is PlateMate?
+								</h1>
+								<p className="poppin">
+									PlateMate is a free web application dedicated to bringing the
+									rich and flavorful world of Filipino cuisine to your home
+									kitchen. Designed to provide personalized recipe
+									recommendations based on the ingredients you currently have,
+									PlateMate simplifies meal planning and cooking. Discover
+									creative and delicious Filipino recipes that match the
+									ingredients in your kitchen, making every meal a delightful
+									journey into the heart of Filipino culinary traditions.
+								</p>
+							</div>
+							<div className="desc p-4 rounded-md bg-white shadow">
+								<h1 className="text-2xl font-semibold text-green-500">
+									How to use PlateMate?
+								</h1>
+								<p className="poppin">
+									Discover delightful Filipino recipes effortlessly by typing
+									your available ingredients into PlateMate&apos;s search bar,
+									and let the app generate personalized recommendations tailored
+									to your kitchen inventory.
+								</p>
+							</div>
+							<div className="desc p-4 rounded-md bg-white shadow">
+								<h1 className="text-2xl font-semibold text-green-500">
+									Features
+								</h1>
+								<p className="poppin">
+									PlateMate offers a seamless cooking experience through
+									features such as user accounts, ingredient inventory
+									management, search and filter options, detailed recipe
+									information, save and share functionalities, shopping list
+									integration, and a responsive design for user-friendly
+									accessibility across devices.
+								</p>
+							</div>
+						</div>
 					)}
 				</div>
-				<div
-					id="recipeBox"
-					className="grid grid-cols-1 px-4 pt-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8 sm:gap-8 md:gap-8 lg:gap-6 xl:gap-6 items-start justify-center">
-					{displayProto}
-				</div>
-				{displayProto.length === 0 && (
-					<div
-						className="flex flex-col w-5/6 items-center gap-6 lg:w-4/6"
-						id="descBox">
-						<div className="desc p-4 rounded-md bg-white shadow">
-							<h1 className="text-2xl font-bold text-green-500">
-								What is PlateMate?
-							</h1>
-							<p className="poppin">
-								PlateMate is a free web application dedicated to bringing the
-								rich and flavorful world of Filipino cuisine to your home
-								kitchen. Designed to provide personalized recipe recommendations
-								based on the ingredients you currently have, PlateMate
-								simplifies meal planning and cooking. Discover creative and
-								delicious Filipino recipes that match the ingredients in your
-								kitchen, making every meal a delightful journey into the heart
-								of Filipino culinary traditions.
-							</p>
-						</div>
-						<div className="desc p-4 rounded-md bg-white shadow">
-							<h1 className="text-2xl font-bold text-green-500">
-								How to use PlateMate?
-							</h1>
-							<p className="poppin">
-								Discover delightful Filipino recipes effortlessly by typing your
-								available ingredients into PlateMate&apos;s search bar, and let
-								the app generate personalized recommendations tailored to your
-								kitchen inventory.
-							</p>
-						</div>
-						<div className="desc p-4 rounded-md bg-white shadow">
-							<h1 className="text-2xl font-bold text-green-500">Features</h1>
-							<p className="poppin">
-								PlateMate offers a seamless cooking experience through features
-								such as user accounts, ingredient inventory management, search
-								and filter options, detailed recipe information, save and share
-								functionalities, shopping list integration, and a responsive
-								design for user-friendly accessibility across devices.
-							</p>
-						</div>
-					</div>
-				)}
 			</div>
 		</>
 	);
